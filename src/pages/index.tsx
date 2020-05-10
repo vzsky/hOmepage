174cf5ea1bd3c0fa23bcf055e42f964aed55ff0a
Touch Sungkawichai <talay@10ninox.com>
2020-05-11 00:40:00 +0700

    auto change mode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,23 @@ import About from './About'
 import Gallery from './Gallery'
 import Work from './Work'
 
-import { Box } from '@chakra-ui/core'
+import { Box, useColorMode } from '@chakra-ui/core'
 import { useEffect, useState } from 'react'
 import { Loading } from '../helper'
 
 const Home = () => {
   const [state, setState] = useState('loading')
+  const { toggleColorMode, colorMode } = useColorMode()
+
+  const setMode = (mode: 'dark' | 'light') => {
+    if (colorMode === mode) return
+    toggleColorMode()
+  }
+
   useEffect(() => {
+    let now = new Date().getHours()
+    let isDay = now > 6 && now < 20
+    setMode(isDay ? 'light' : 'dark')
     setState('ready')
   }, [])
   if (state === 'loading') return <Loading />
